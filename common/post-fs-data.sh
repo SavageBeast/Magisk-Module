94d588f7691e359c91cfff129836af8d72790a50
Your Name <you@example.com>
2017-09-11 20:57:39 +0530

    Fallback for setting fingerprint

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,4 +5,4 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
-#$RESETPROP "ro.build.fingerprint" "motorola/osprey_retasia_ds_2gb/osprey_ud2:6.0.1/MPIS24.107-55-2-12/14:user/release-keys"
+$RESETPROP "ro.build.fingerprint" "motorola/osprey_retasia_ds_2gb/osprey_ud2:6.0.1/MPIS24.107-55-2-12/14:user/release-keys"
